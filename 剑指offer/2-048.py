c987963c9f138099fba92e628eb89d9707f621d7
汤陆明远 <tanglujay@outlook.com>
2022-04-22 20:33:21 +0800

    day8

---
+++ 剑指offer/2-048.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        def dfs(root: TreeNode, prevTotal: int) -> int:
+            if not root:
+                return 0
+            total = prevTotal * 10 + root.val
+            if not root.left and not root.right:
+                return total
+            else:
+                return dfs(root.left, total) + dfs(root.right, total)
+
+        return dfs(root, 0)
